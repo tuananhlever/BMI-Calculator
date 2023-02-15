78316da5c241b1393e7b9fbd853bd25880219bd9
tuananhlever <82800727+tuananhlever@users.noreply.github.com>
2023-02-15 16:55:02 +0700

    Update CalculatorBrain.swift

--- BMI-Calculator/CalculatorBrain.swift
+++ BMI-Calculator/CalculatorBrain.swift
@@ -1,10 +1,3 @@
-//
-//  CalculatorBrain.swift
-//  BMI Calculator
-//
-//  Created by macbook on 14/02/2023.
-//  Copyright © 2023 Angela Yu. All rights reserved.
-//
 
 import Foundation
 struct CalculatorBrain {
